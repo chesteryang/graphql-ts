c3181ca64a3f08720f430cb2bc8cde562a41814c
Chester Yang <chester_cyyang@hotmail.com>
2018-09-10 12:04:28 -0600

    commit for compile

---
+++ src/resolvers.ts
@@ -0,0 +1,13 @@
+import { ResolverMap } from "./types/graphql-utils";
+
+export const resolvers: ResolverMap = {
+    Query: {
+        hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
+      },
+    
+    Mutation: {
+        register: (_:any, { email, password }: GQL.IRegisterOnMutationArguments) => {
+            return email + password
+         }
+      }
+}
