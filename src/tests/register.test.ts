ef7d9ee803253ff56f271640045187c4c78868ff
Chester Yang <chester_cyyang@hotmail.com>
2018-09-10 14:57:38 -0600

    add jest, in memory db test

---
+++ src/tests/register.test.ts
@@ -0,0 +1,19 @@
+import { createConnection } from "typeorm";
+import { User } from "../entity/User";
+function sum(a:number, b:number) {
+    return a + b;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('in memory database', async () => {
+    const email = "test@gmail.com";
+    await createConnection({type: "sqlite", database: ":memory:", entities: [User], logging: true, synchronize: true})
+    const user = User.create({email, password: "asdfgh"});
+    await user.save();
+    const savedUser = await User.find({ where: { email } });
+    expect(savedUser).toHaveLength(1);
+    expect(savedUser[0].email).toEqual(email);
+})
