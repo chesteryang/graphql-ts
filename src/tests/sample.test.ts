e76f6e38f3f614540e5daedeac73b54c98df6db8
Chester Yang <chester_cyyang@hotmail.com>
2018-09-12 15:23:24 -0600

    test inserting

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -1,23 +1,51 @@
-import { createConnection, ConnectionOptions } from "typeorm";
+import { createConnection, ConnectionOptions, Connection } from "typeorm";
 import { User } from "../entity/User";
 import { Customer } from "../chinookEntities/Customer";
 import { Employee } from "../chinookEntities/Employee";
+import { Artist } from "../chinookEntities/Artist";
 
 const options: ConnectionOptions = {
     type: "sqlite", 
     database: ":memory:", 
-    entities: [User, Customer, Employee], 
+    entities: [User, Customer, Employee, Artist], 
     logging: false, 
     synchronize: true
 }
+let connection: Connection; 
+const getConnection = async () => {
+    if(!connection){
+        connection = await createConnection(options) 
+    }
+    return connection;
+}
+
+afterAll(() => {
+    if(connection){
+        connection.close()
+    }
+})
+
 test('in memory database', async () => {
     const email = "test@gmail.com";
-    await createConnection(options)
+    await getConnection();
     const user = User.create({email, password: "asdfgh"});
     await user.save();
     const savedUser = await User.find({ where: { email } });
     expect(savedUser).toHaveLength(1);
     expect(savedUser[0].email).toEqual(email);
+})
+
+test("add Artist works", async () => {
+    const name = "Paul Mauriat";
+    const ctn = await getConnection();
+    const repo = ctn.getRepository(Artist);
+    const artist = repo.create({Name: name})
+    const artist1 = repo.create({Name: name})
+    await repo.save(artist);
+    await repo.save(artist1);
+    const saved = await repo.find({where: {Name: name}});
+    expect(saved).toHaveLength(2);
+    expect(saved[0].Name).toEqual(name);
 })
 
 function sum(a:number, b:number) {
