2ade112c01b15862df8ef848166e2957a8cccf07
Chester Yang <chester_cyyang@hotmail.com>
2018-09-12 14:41:53 -0600

    add in memory tests

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -1,9 +1,18 @@
-import { createConnection } from "typeorm";
+import { createConnection, ConnectionOptions } from "typeorm";
 import { User } from "../entity/User";
+import { Customer } from "../chinookEntities/Customer";
+import { Employee } from "../chinookEntities/Employee";
 
+const options: ConnectionOptions = {
+    type: "sqlite", 
+    database: ":memory:", 
+    entities: [User, Customer, Employee], 
+    logging: true, 
+    synchronize: true
+}
 test('in memory database', async () => {
     const email = "test@gmail.com";
-    await createConnection({type: "sqlite", database: ":memory:", entities: [User], logging: false, synchronize: true})
+    await createConnection(options)
     const user = User.create({email, password: "asdfgh"});
     await user.save();
     const savedUser = await User.find({ where: { email } });
