c9efee98bf5b6eabd7c85eca6276f1c894cb0d17
Chester Yang <chester_cyyang@hotmail.com>
2018-09-13 13:53:36 -0600

    add yug validation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,39 +1,63 @@
 import * as bcrypt from "bcryptjs"
+import * as yup from "yup";
 import { ResolverMap } from "./types/graphql-utils"
-import {GQL} from "./types/schema"
+import { GQL } from "./types/schema"
 import { User } from "./entity/User"
+import { formatYupError } from "./utils/formatYupError";
+import {
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough,
+  duplicateEmail
+} from "./utils/errorMessages";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+});
 
 export const resolvers: ResolverMap = {
-    Query: {
-        hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
-      },
-    
-    Mutation: {
-      register: async (
-        _,
-        { email, password }: GQL.IRegisterOnMutationArguments
-      ) => {
-        const userAlreadyExists = await User.findOne({
-          where: { email },
-          select: ["id"]
-        });
-  
-        if (userAlreadyExists) {
-          return [
-            {
-              path: "email",
-              message: "already taken"
-            }
-          ];
-        }
+  Query: {
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
+  },
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = User.create({
-          email,
-          password: hashedPassword
-        });
-        await user.save();
-        return null;
+  Mutation: {
+    register: async (_, args: GQL.IRegisterOnMutationArguments) => {
+      try {
+        await schema.validate(args, { abortEarly: false });
+      } catch (err) {
+        return formatYupError(err);
       }
+
+      const { email, password } = args;
+      const userAlreadyExists = await User.findOne({
+        where: { email },
+        select: ["id"]
+      });
+
+      if (userAlreadyExists) {
+        return [
+          {
+            path: "email",
+            message: duplicateEmail
+          }
+        ];
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+      await user.save();
+      return null;
     }
+  }
 }
