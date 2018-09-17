bc132cf67c165030d8694d38d01572e9834e2095
Chester Yang <chester_cyyang@hotmail.com>
2018-09-17 15:59:31 -0600

    refactoring tests

--- src/tests/chinook/ChinookEntityManager.test.ts
+++ src/tests/chinook/ChinookEntityManager.test.ts
@@ -1,20 +1,22 @@
 import { ChinookContext } from "../../chinookEntities/ChinookContext";
 import { Employee } from "../../chinookEntities/Employee";
 import { expectList } from "../testHelper";
+import { EntityManager } from "typeorm";
 
-const context: ChinookContext = ChinookContext.Instance;
+let manager: EntityManager;
+
+beforeAll(async () => {
+    const conn = await ChinookContext.Instance.getChinookConnection();
+    manager = conn.manager;
+})
 
 describe("using entity manager", () => {
     it("can query employee list", async () => {
-        const conn = await context.getChinookConnection();
-        const manager = conn.manager;
         const list: Employee[] = await manager.find(Employee);
         expectList(list);
     })
 
     it("can query general manager", async () => {
-        const conn = await context.getChinookConnection();
-        const manager = conn.manager;
         const list: Employee[] = await manager.find(Employee, {where: {ReportsTo: null}});
         expectList(list);
         expect(list[0].Title).toEqual("General Manager");
